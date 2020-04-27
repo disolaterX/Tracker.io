5466e2802ab92bfa9eb072d830b28c4e8699e6c9
Kartik Yadav <dev.kartik98@gmail.com>
2020-04-27 13:32:46 +0530

    first working commit

--- routes.js
+++ routes.js
@@ -1,10 +1,40 @@
 
+const fs = require('fs');
+
 module.exports = (app) => {
   app.get("/", (req, res) => {
     res.sendFile('tracker.html', { root: `${__dirname}/api` });
   })
   app.post("/update", (req, res) => {
-    console.log(req.body);
-    res.json(req.body)
+    const { username } = req.body
+    delete req.body.username
+    fs.readFile('api/results.json', 'utf8', function readFileCallback(err, data) {
+      if (err) {
+        console.log(err);
+      } else {
+        obj = JSON.parse(data); //now it an object
+        if (!obj[username]) {
+          obj[username] = []
+        }
+        obj[username].push(req.body); //add some data
+        json = JSON.stringify(obj); //convert it back to json
+        fs.writeFile('api/results.json', json, 'utf8', () => { }); // write it back 
+      }
+    });
+    res.sendStatus(200)
+  })
+  app.post("/get", (req, res) => {
+    const { username } = req.body
+
+    fs.readFile('api/results.json', 'utf8', function readFileCallback(err, data) {
+      if (err) {
+        console.log(err);
+        res.json({ error: err })
+      } else {
+        obj = JSON.parse(data);
+        res.json(obj[username])
+      }
+    })
+
   })
 }
